b8fc03ec99504fb51fc31f4ee399089445705ac1
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-09-06 20:03:03 +0530

    Render chess board squares with Chess.js

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,1 +1,38 @@
+
 const socket = io()
+
+const chess = new Chess()  // init chess game now
+
+const boardElement = document.querySelector(".chessboard")
+
+
+let draggedPiece = null
+let sourceSquare = null
+let playerRole = null
+
+const renderBoard = () => {
+    const board = chess.board()
+    boardElement.innerHTML = ""
+    board.forEach((row, rowindex) =>{
+        row.forEach((square, squareindex) =>{
+            // console.log(square)
+            const squareElement = document.createElement("div")
+            squareElement.classList.add(
+                "sqaure",
+                (rowindex + squareindex) % 2 ==0 ? "light" : "dark"
+            )
+
+            squareElement.dataset.row = rowindex
+            squareElement.dataset.col = squareindex
+        })
+    })
+}
+
+const handleMove = () => {}
+
+const getPieceUnicode = ()=> {}
+
+
+renderBoard()
+handleMove()
+getPieceUnicode()
